941edb713efc6b8c8b7890be9b69fa3a934d430a
GitHubVasilev <87832971+GitHubVasilev@users.noreply.github.com>
2024-01-05 22:13:21 +0300

    Добавлены базовые классы для работы с Autpmapper, (#2)
    Добавлен базовый класс для обертки результата выполнения запроса
    
    Co-authored-by: GitHubVasilev <a.vasile_1995@mail.ru>

---
+++ Blog.Domain/Base/WrapperResult.cs
@@ -0,0 +1,42 @@
+﻿namespace Blog.Domain.Base
+{
+    /// <summary>
+    /// Сингалтон. Базовый класс с данными о результате операции
+    /// </summary>
+    public abstract class WrapperResult
+    {
+        public string? Message { get; set; }
+        public Exception? ExceptionObject { get; set; }
+        public bool IsSuccess
+        {
+            get => ExceptionObject is null;
+        }
+
+        public static WrapperResult Build<T>(T result, string? message = null, Exception? error = null)
+        {
+            return new WrapperResult<T>
+            {
+                Result = result,
+                Message = message,
+                ExceptionObject = error,
+            };
+        }
+
+        public static WrapperResult<T> Build<T>()
+        {
+            return new WrapperResult<T>
+            {
+                Result = default,
+            };
+        }
+    }
+
+    /// <summary>
+    /// Класс с данными о проведении операции
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class WrapperResult<T> : WrapperResult
+    {
+        public T? Result { get; set; }
+    }
+}
