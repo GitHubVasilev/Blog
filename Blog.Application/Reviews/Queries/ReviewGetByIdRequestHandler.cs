fbfb1920480b0e076ad59900977bb13b5788c4d8
avasilev <a.vasilev_1995@mail.ru>
2024-07-10 00:39:36 +0300

    Review методы GetById, Create

---
+++ Blog.Application/Reviews/Queries/ReviewGetByIdRequestHandler.cs
@@ -0,0 +1,44 @@
+﻿using Blog.Application.Common.Exceptions;
+using Blog.Application.Interfaces;
+using Blog.Application.Reviews.ViewModels;
+using Blog.Domain;
+using Blog.Domain.Base;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Application.Reviews.Queries;
+
+public record ReviewGetByIdRequest(Guid ReviewId, ClaimsPrincipal User)
+    : IRequest<WrapperResult<ReviewGetViewModel>>;
+
+public class ReviewGetByIdRequestHandler : IRequestHandler<ReviewGetByIdRequest, WrapperResult<ReviewGetViewModel>>
+{
+    private readonly IBlogDbContext _dbContext;
+    private readonly ICustomMapper<Review, ReviewGetViewModel> _mapper;
+    public ReviewGetByIdRequestHandler(IBlogDbContext dbContext, ICustomMapper<Review, ReviewGetViewModel> mapper)
+    {
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
+
+    public async Task<WrapperResult<ReviewGetViewModel>> Handle(ReviewGetByIdRequest request, CancellationToken cancellationToken)
+    {
+        var result = new WrapperResult<ReviewGetViewModel>();
+        var model = await _dbContext.Reviews.FirstOrDefaultAsync(m => m.Id == request.ReviewId, cancellationToken);
+
+        if (model is null)
+        {
+            result.ExceptionObject = new BlogEntityNotFoundException($"Отзыв с ID {request.ReviewId} не найден", request.ReviewId);
+            return result;
+        }
+
+        result.Result = _mapper.ToViewModel(model);
+        return result;
+    }
+}
---
+++ Blog.Application/Reviews/Queries/ReviewGetForCreateRequestHandler.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Application.Reviews.ViewModels;
+using Blog.Domain.Base;
+using MediatR;
+using System.Security.Claims;
+
+namespace Blog.Application.Reviews.Queries;
+
+public record ReviewGetForCreateRequest(ClaimsPrincipal User) 
+    : IRequest<WrapperResult<ReviewCreateViewModel>>;
+public class ReviewGetForCreateRequestHandler : IRequestHandler<ReviewGetForCreateRequest, WrapperResult<ReviewCreateViewModel>>
+{
+    public async Task<WrapperResult<ReviewCreateViewModel>> Handle(ReviewGetForCreateRequest request, CancellationToken cancellationToken)
+    {
+        return WrapperResult.Build<ReviewCreateViewModel>();
+    }
+}
